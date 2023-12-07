18b155c7243b0a75a260f451a60d1a9283d24e01
José Sánchez <jsanchezn99@gmail.com>
2023-12-07 16:04:12 +0100

    cambio guardar en log evolucionCajeros.txt

--- src/main/java/SistemaBancario/Cajero.java
+++ src/main/java/SistemaBancario/Cajero.java
@@ -2,6 +2,9 @@ package SistemaBancario;
 
 //@author Héctor Benavente García
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Random;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
@@ -15,6 +18,8 @@ public class Cajero implements Runnable {
     private int dinero;
     private Persona persona;
     private Thread[] operarios;
+    private String archivo = "evolucionCajeros.txt";
+    private String mensajeLog = "";
     
     public Cajero(int id, Thread[] operarios, JTextField total, JTextField operando, JTextArea movimientos) {
         this.id = id;
@@ -28,6 +33,18 @@ public class Cajero implements Runnable {
     public synchronized void insertar(int cantidad) throws InterruptedException{
         operando.setText(persona.getId()+"-I+"+persona.getDinero());
         movimientos.append(persona.getId()+"-I+"+persona.getDinero()+"\n");
+        mensajeLog = persona.getId()+"-I+"+persona.getDinero()+"\n";
+        try{
+            FileWriter fileWriter = new FileWriter(archivo, true); // El parámetro true indica que se añadirá al final del archivo
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            bufferedWriter.write(mensajeLog);
+            bufferedWriter.newLine(); // Añadir nueva línea para el próximo registro
+            bufferedWriter.close();
+            System.out.println(mensajeLog + "guardado en log");
+            System.out.println("Registro guardado correctamente en el archivo.");
+        }catch (IOException e){
+            System.err.println("Error al escribir en el log: " + e.getMessage());
+        }
         Random rand = new Random();
         Thread.sleep(rand.nextInt(2000)+2000);
         int nuevoDinero = dinero + cantidad;
@@ -41,6 +58,18 @@ public class Cajero implements Runnable {
     public synchronized void extraer(int cantidad) throws InterruptedException{
         operando.setText(persona.getId()+"-E-"+persona.getDinero());
         movimientos.append(persona.getId()+"-E-"+persona.getDinero()+"\n");
+        mensajeLog = persona.getId()+"-E-"+persona.getDinero()+"\n";
+        try{
+            FileWriter fileWriter = new FileWriter(archivo, true); // El parámetro true indica que se añadirá al final del archivo
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            bufferedWriter.write(mensajeLog);
+            bufferedWriter.newLine(); // Añadir nueva línea para el próximo registro
+            bufferedWriter.close();
+            System.out.println(mensajeLog + "guardado en log");
+            System.out.println("Registro guardado correctamente en el archivo.");
+        }catch (IOException e){
+            System.err.println("Error al escribir en el log: " + e.getMessage());
+        }
         Random rand = new Random();
         Thread.sleep(rand.nextInt(2000)+2500);
         int nuevoDinero = dinero - cantidad;
